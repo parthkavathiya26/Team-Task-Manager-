88a3e40f9334428261496a32ea24faf5f777102a
parthkavathiya26 <pckavathiya1@gmail.com>
2026-05-02 11:42:51 +0530

    fix login api

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -9,14 +9,15 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("/api/auth/login", {
         email,
         password,
       });
 
       localStorage.setItem("token", res.data.token);
       nav("/dashboard");
-    } catch {
+    } catch (err) {
+      console.log(err);
       alert("Login failed");
     }
   };
@@ -25,10 +26,17 @@ export default function Login() {
     <div style={{ padding: 40 }}>
       <h2>Login</h2>
 
-      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <br /><br />
 
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <br /><br />
 
       <button onClick={handleLogin}>Login</button>
